c718556d1ffc43919068a8d3a623b34e4185c87f
seifange <seifang@wix.com>
2020-03-21 15:44:56 +0200

    add option select category from list

--- src/Views/UsefulComponent/AutoComplete.jsx
+++ src/Views/UsefulComponent/AutoComplete.jsx
@@ -1,16 +1,27 @@
 /* eslint-disable no-use-before-define */
-import React from 'react';
-import TextField from '@material-ui/core/TextField';
-import Autocomplete from '@material-ui/lab/Autocomplete';
+import React from "react";
+import TextField from "@material-ui/core/TextField";
+import Autocomplete from "@material-ui/lab/Autocomplete";
 
 export default function ComboBox(props) {
+  const [value, setValue] = React.useState(null);
+
+  const handleChange = (event, newValue) => {
+    props.setName(newValue.title);
+    setValue(newValue);
+  };
+
   return (
     <Autocomplete
-      id="combo-box-demo"
+      value={value}
+      id={props.id}
       options={props.items}
       getOptionLabel={option => option.title}
       style={{ width: 400 }}
-      renderInput={params => <TextField {...params} label={props.boxLabel} variant="outlined" />}
+      renderInput={params => (
+        <TextField {...params} label={props.boxLabel} variant="outlined" />
+      )}
+      onChange={handleChange}
     />
   );
 }
