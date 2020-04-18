3b4e4f5d27354e63ed576e126aa9c2992df4d727
maimonav <maimonav@post.bgu.ac.il>
2020-04-18 20:04:32 +0300

    added some tests

--- cypress/tests/Acceptance tests/manageMovies.spec.js
+++ cypress/tests/Acceptance tests/manageMovies.spec.js
@@ -14,53 +14,54 @@ const user = "admin";
 const movie = "movie";
 const key = "key";
 const examinationRoom = "examinationRoom";
+
 context("Manage Movies", () => {
   beforeEach(() => {
     cy.startSystem();
     cy.login(user, user);
   });
+  //Will be tested when we have tests for add movie
+  // it("edit movie", () => {
+  //   cy.accessTab(inventoryActionsTabHook);
+  //   cy.accessTab(moviesTabHook);
+  //   cy.chooseAction(editActionHook);
 
-  it("edit movie", () => {
-    cy.accessTab(inventoryActionsTabHook);
-    cy.accessTab(moviesTabHook);
-    cy.chooseAction(editActionHook);
+  //   cy.get(`[data-hook=${movieNameHook}`)
+  //     .click()
+  //     .type(movie)
+  //     .type("{downarrow}")
+  //     .type("{enter}");
 
-    cy.get(`[data-hook=${movieNameHook}`)
-      .click()
-      .type(movie)
-      .type("{downarrow}")
-      .type("{enter}");
+  //   cy.get(`[data-hook=${categoryNameHook}]`)
+  //     .click()
+  //     .type(movie)
+  //     .type("{downarrow}")
+  //     .type("{enter}");
 
-    cy.get(`[data-hook=${categoryNameHook}]`)
-      .click()
-      .type(movie)
-      .type("{downarrow}")
-      .type("{enter}");
+  //   cy.get(`[data-hook=${keyHook}]`)
+  //     .click()
+  //     .type(key);
 
-    cy.get(`[data-hook=${keyHook}]`)
-      .click()
-      .type(key);
+  //   cy.get(`[data-hook=${examinationRoomHook}]`)
+  //     .click()
+  //     .type(examinationRoom);
 
-    cy.get(`[data-hook=${examinationRoomHook}]`)
-      .click()
-      .type(examinationRoom);
+  //   cy.get(`[data-hook=${actionButtonHook}]`).click();
+  // });
 
-    cy.get(`[data-hook=${actionButtonHook}]`).click();
-  });
+  // it("remove movie", () => {
+  //   cy.accessTab(inventoryActionsTabHook);
+  //   cy.accessTab(moviesTabHook);
+  //   cy.chooseAction(removeActionHook);
 
-  it("remove movie", () => {
-    cy.accessTab(inventoryActionsTabHook);
-    cy.accessTab(moviesTabHook);
-    cy.chooseAction(removeActionHook);
+  //   cy.get(`[data-hook=${movieNameHook}`)
+  //     .click()
+  //     .type(movie)
+  //     .type("{downarrow}")
+  //     .type("{enter}");
 
-    cy.get(`[data-hook=${movieNameHook}`)
-      .click()
-      .type(movie)
-      .type("{downarrow}")
-      .type("{enter}");
-
-    cy.get(`[data-hook=${actionButtonHook}]`).click();
-  });
+  //   cy.get(`[data-hook=${actionButtonHook}]`).click();
+  // });
 
   afterEach(() => {
     cy.logout();
---
+++ cypress/tests/Unit tests/manageMovies.spec.js
@@ -0,0 +1,87 @@
+import {
+  inventoryActionsTabHook,
+  movieNameHook,
+  categoryNameHook,
+  actionButtonHook,
+  editActionHook,
+  removeActionHook,
+  moviesTabHook,
+  keyHook,
+  examinationRoomHook
+} from "../../../src/consts/data-hooks";
+
+const user = "admin";
+const movie = "movie";
+const key = "key";
+const examinationRoom = "examinationRoom";
+
+context("Type all fields", () => {
+  beforeEach(() => {
+    cy.startSystem();
+    cy.login(user, user);
+  });
+  it("edit movie", () => {
+    cy.accessTab(inventoryActionsTabHook);
+    cy.accessTab(moviesTabHook);
+    cy.chooseAction(editActionHook);
+
+    cy.get(`[data-hook=${movieNameHook}`)
+      .click()
+      .type(movie)
+      .type("{esc}");
+
+    cy.get(`[data-hook=${categoryNameHook}]`)
+      .click()
+      .type(movie)
+      .type("{esc}");
+
+    cy.get(`[data-hook=${keyHook}]`)
+      .click()
+      .type(key);
+
+    cy.get(`[data-hook=${examinationRoomHook}]`)
+      .click()
+      .type(examinationRoom);
+  });
+
+  it("remove movie", () => {
+    cy.accessTab(inventoryActionsTabHook);
+    cy.accessTab(moviesTabHook);
+    cy.chooseAction(removeActionHook);
+
+    cy.get(`[data-hook=${movieNameHook}`)
+      .click()
+      .type(movie)
+      .type("{esc}");
+
+    cy.get(`[data-hook=${actionButtonHook}]`).click();
+  });
+
+  afterEach(() => {
+    cy.logout();
+  });
+});
+
+context("Click all buttons", () => {
+  beforeEach(() => {
+    cy.startSystem();
+    cy.login(user, user);
+  });
+  it("edit movie", () => {
+    cy.accessTab(inventoryActionsTabHook);
+    cy.accessTab(moviesTabHook);
+    cy.chooseAction(editActionHook);
+    cy.get(`[data-hook=${actionButtonHook}]`).click();
+  });
+
+  it("remove movie", () => {
+    cy.accessTab(inventoryActionsTabHook);
+    cy.accessTab(moviesTabHook);
+    cy.chooseAction(removeActionHook);
+    cy.get(`[data-hook=${actionButtonHook}]`).click();
+  });
+
+  afterEach(() => {
+    cy.logout();
+  });
+});
